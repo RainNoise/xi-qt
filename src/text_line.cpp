709e2b3e331daab7ddfcd5bd381c6b24022a7b08
sw5cc <sw5cc.125pflops@gmail.com>
2018-07-24 20:15:05 +0800

    update

--- src/text_line.cpp
+++ src/text_line.cpp
@@ -13,75 +13,86 @@ TextLine::TextLine(const QString &text, const std::shared_ptr<Font> &font) {
 }
 
 int TextLine::xToIndex(qreal x) {
-	QString text = m_text;
-	auto width = 0.f;
-	auto idx = -1;
-	for (auto i = 0; i < text.size(); ++i) {
-		width = m_fontMetrics->width(text.mid(0, i));
-		if (width >= x - m_fontMetrics->averageCharWidth()/2) {
-			idx = i;
-			break;
-		}
-	}
-	if (idx == 0) return 0;
-	if (idx == -1) return m_text.toUtf8().length();
+    QString text = m_text;
+    auto width = 0.f;
+    auto idx = -1;
+    for (auto i = 0; i < text.size(); ++i) {
+        width = m_fontMetrics->width(text.mid(0, i));
+        if (width >= x - m_fontMetrics->averageCharWidth() / 2) {
+            idx = i;
+            break;
+        }
+    }
+    if (idx == 0) return 0;
+    if (idx == -1) return m_text.toUtf8().length();
 
-	auto utf8Idx = text.mid(0, idx).toUtf8().length();
-	return utf8Idx;
+    auto utf8Ix = text.mid(0, idx).toUtf8().length();
+    return utf8Ix;
 
-	//auto line = m_layout->lineAt(0);
-	//auto utf8Idx = m_text.mid(0, line.xToCursor(x)).toUtf8().length();
-	//return utf8Idx;
+    // TODO: IMPROVE
+    //if (m_layout->lineCount() > 0) {
+    //    auto innerLine = m_layout->lineAt(0);
+    //    auto idx = innerLine.xToCursor(x);
+    //    auto utf8Ix = m_text.mid(0, idx).toUtf8().length();
+    //    return utf8Ix;
+    //}
+    //return 0;
 }
 
 qreal TextLine::indexTox(int ix) {
     if (ix < 0 || ix > m_text.size()) { return 0; }
-    auto u8str = m_text.toUtf8();
-    return m_fontMetrics->width(u8str.left(ix));
+    return m_fontMetrics->width(m_text.toUtf8().left(ix));
+
+    // TODO: IMPROVE
+    //if (m_layout->lineCount() > 0) {
+    //    auto innerLine = m_layout->lineAt(0);
+    //    auto utf8ix = m_text.toUtf8().mid(0, ix).length();
+    //    auto x = innerLine.cursorToX(utf8ix);
+    //    return x;
+    //}
+    //return 0;
 }
 
-std::shared_ptr<xi::TextLine> TextLineBuilder::build()
-{
-	auto textline = std::make_shared<TextLine>(m_text, m_font);
-	int leading = textline->metrics()->leading();
-	auto lineWidth = textline->metrics()->width(m_text); // slow
-	//auto lineWidth = 10000;
-	qreal height = 0;
-	QVector<QTextLayout::FormatRange> overrides;
-	QTextLayout::FormatRange  fmt;
-	QTextCharFormat cfmt;
-	cfmt.setFont(m_font->getFont());
-	cfmt.setForeground(QColor::fromRgb(248, 248, 186)); // 255, 215, 0 // 255, 128, 0 // 189, 183, 107 // 248, 248, 186
-	fmt.start = 0;
-	fmt.length = m_text.length();
-	fmt.format = cfmt;
-	overrides.push_back(fmt);
-	textline->layout()->setFormats(overrides);
-	textline->layout()->setCacheEnabled(true);
-	textline->layout()->beginLayout();
-	while (1) {
-		QTextLine qline = textline->layout()->createLine();
-		if (!qline.isValid())
-			break;
+std::shared_ptr<xi::TextLine> TextLineBuilder::build() {
+    auto textline = std::make_shared<TextLine>(m_text, m_font);
+    int leading = textline->metrics()->leading();
+    auto lineWidth = textline->metrics()->width(m_text); // slow
+    //auto lineWidth = 10000;
+    qreal height = 0;
+    QVector<QTextLayout::FormatRange> overrides;
+    QTextLayout::FormatRange fmt;
+    QTextCharFormat cfmt;
+    cfmt.setFont(m_font->getFont());
+    cfmt.setForeground(QColor::fromRgb(248, 248, 186)); // 255, 215, 0 // 255, 128, 0 // 189, 183, 107 // 248, 248, 186
+    fmt.start = 0;
+    fmt.length = m_text.length();
+    fmt.format = cfmt;
+    overrides.push_back(fmt);
+    textline->layout()->setFormats(overrides);
+    textline->layout()->setCacheEnabled(true);
+    textline->layout()->beginLayout();
+    while (1) {
+        QTextLine qline = textline->layout()->createLine();
+        if (!qline.isValid())
+            break;
 
-		qline.setLineWidth(lineWidth);
-		height += leading;
-		qline.setPosition(QPointF(0, height));
-		height += qline.height();
-	}
-	textline->layout()->endLayout();
+        qline.setLineWidth(lineWidth);
+        height += leading;
+        qline.setPosition(QPointF(0, height));
+        height += qline.height();
+    }
+    textline->layout()->endLayout();
 
-	return textline;
+    return textline;
 }
 
-void Painter::drawLineBg(QPainter &painter, const std::shared_ptr<TextLine> &line, qreal x, const RangeF &y)
-{
-	auto selRanges = line->selRanges();
-	foreach(const SelRange selRange, *selRanges) {
-		QRect rect;
-		selRange.range;
-		painter.fillRect(rect, selRange.color);
-	}
+void Painter::drawLineBg(QPainter &painter, const std::shared_ptr<TextLine> &line, qreal x, const RangeF &y) {
+    auto selRanges = line->selRanges();
+    foreach (const SelRange selRange, *selRanges) {
+        QRect rect;
+        selRange.range;
+        painter.fillRect(rect, selRange.color);
+    }
 }
 
 } // namespace xi
